6cab14eafd2419ab4e984b81160828b0fd1271cf
pangorithm <phs559800@gmail.com>
2024-06-17 21:19:56 +0900

    panic! 호출 대신 Result 반환하기

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -21,13 +21,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
