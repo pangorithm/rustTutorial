48a5a0f7f1f526009a444918d7c93b0c0add8ab7
pangorithm <phs559800@gmail.com>
2024-07-08 21:31:41 +0900

    작업을 기다리는 스레드에게 정지 신호 보내기

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -5,49 +5,47 @@ use std::{
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-  /// Create a new ThreadPool.
-  ///
-  /// The size is the number of threads in the pool.
-  ///
-  /// # Panics
-  ///
-  /// The `new` function will panic if the size is zero.
-  pub fn new(size: usize) -> ThreadPool {
-    assert!(size > 0);
+    pub fn new(size: usize) -> ThreadPool {
 
-    let (sender, receiver) = mpsc::channel();
+        assert!(size > 0);
 
-    let receiver = Arc::new(Mutex::new(receiver));
+        let (sender, receiver) = mpsc::channel();
 
-    let mut workers = Vec::with_capacity(size);
+        let receiver = Arc::new(Mutex::new(receiver));
 
-    for id in 0..size {
-      workers.push(Worker::new(id, Arc::clone(&receiver)));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
-    ThreadPool { workers, sender }
-  }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
 
-
-  pub fn execute<F>(&self, f: F)
-  where
-      F: FnOnce() + Send + 'static,
-  {
-      let job = Box::new(f);
-
-      self.sender.send(job).unwrap();
-  }
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
 
 }
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        drop(self.sender.take());
+
         for worker in &mut self.workers {
             println!("Shutting down worker {}", worker.id);
 
@@ -66,11 +64,19 @@ struct Worker {
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job; executing.");
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
 
-            job();
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
 
         Worker {
