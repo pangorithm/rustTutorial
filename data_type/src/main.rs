eac520243128c45ed5d42ba0f73a81d1ef23fe88
pangorithm <phs559800@gmail.com>
2024-07-27 21:06:55 +0900

    튜플

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,18 +1,7 @@
 fn main() {
-    // 정수
-    println!("i8: MIN = {}, i8: MAX = {}", i8::MIN, i8::MAX);
-    println!("i16: MIN = {}, i16: MAX = {}", i16::MIN, i16::MAX);
-    println!("i32: MIN = {}, i32: MAX = {}", i32::MIN, i32::MAX);
-    println!("i64: MIN = {}, i64: MAX = {}", i64::MIN, i64::MAX);
-    println!("i128: MIN = {}, i128: MAX = {}", i128::MIN, i128::MAX);
+    let tuple = (1, 2, 3);
+    println!("tuple: {}, {}, {}", tuple.0, tuple.1, tuple.2);
 
-    println!("u8: MIN = {}, u8: MAX = {}", u8::MIN, u8::MAX);
-    println!("u16: MIN = {}, u16: MAX = {}", u16::MIN, u16::MAX);
-    println!("u32: MIN = {}, u32: MAX = {}", u32::MIN, u32::MAX);
-    println!("u64: MIN = {}, u64: MAX = {}", u64::MIN, u64::MAX);
-    println!("u128: MIN = {}, u128: MAX = {}", u128::MIN, u128::MAX);
-
-    // 부동소수점형
-    println!("f32: MIN = {}, f32: MAX = {}", f32::MIN, f32::MAX);
-    println!("f64: MIN = {}, f64: MAX = {}", f64::MIN, f64::MAX);
+    let (x, y, z): (i32, char, bool) = (1, 'a', true);
+    println!("x={}, y={}, z={}", x, y, z);
 }
