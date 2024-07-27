6ec569397d77ed9aa5103b123b7d4e6da12c7cf8
pangorithm <phs559800@gmail.com>
2024-07-28 00:01:02 +0900

    구조체로 연결리스트 만들기

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,14 +1,45 @@
-fn print_optional(val: Option<String>) {
-    match val {
-        Some(val) => println!("{}", val),
-        None => println!("None"),
+struct Node {
+    value: i32,              // 노드의 값
+    next: Option<Box<Node>>, // Option을 사용해 다음 노드가 없는 상황을 처리
+}
+
+impl Node {
+    fn append(&mut self, elem: i32) {
+        match self.next {
+            Some(ref mut next) => {
+                // 값이 있는 경우
+                next.append(elem); // 마지막 노드를 찾기 위해 다음 노드로 이동
+            }
+            None => {
+                // 값이 없을 경우 마지막 노드로 간주
+                let node = Node {
+                    // 마지막 노드에 값을 삽입
+                    value: elem,
+                    next: None,
+                };
+                self.next = Some(Box::new(node))
+            }
+        }
+    }
+
+    fn list(&self) {
+        print!("{},", self.value);
+        match self.next {
+            Some(ref next) => next.list(), // 다음 노드로 이동
+            None => {}
+        }
     }
 }
 
 fn main() {
-    let some_string = Some(String::from("러스트"));
-    let none_string: Option<String> = None;
+    let mut head = Node {
+        value: 1,
+        next: None,
+    };
 
-    print_optional(some_string);
-    print_optional(none_string);
+    for i in 2..10 {
+        head.append(i);
+    }
+
+    head.list();
 }
