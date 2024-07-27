952dffa98e5fb4c237d1fedb726a99590809c841
pangorithm <phs559800@gmail.com>
2024-07-27 23:07:26 +0900

    구조체 정의

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,24 +1,38 @@
+use std::io;
+
 fn main() {
-    // 클로저 기초 예제
-    let mut x = 5;
-    let mut inc = || {
-        x += 1;
-    }; // 할당문이므로 ; 이 필요하다
-    inc();
-    println!("변수 x: {}", x);
+    println!("학번을 입력하세요");
+    let mut id = String::new();
+    io::stdin().read_line(&mut id);
+    let id: i32 = id.trim().parse().unwrap();
 
-    // 클로저에 파라미터 정의
-    let x = 10;
-    let add = |y| x + y; // add는 클로저 함수가 된다.
-    println!("10+5={}", add(5));
+    println!("이름을 입력해주세요.");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name);
+    let name = name.trim().to_string(); // 공백 제거
 
-    // move 키워드를 사용해 클로저의 소유권 이전
-    let s = String::from("Hello");
-    let f = move || {
-        // move 클로저는 소유권을 이전한다.
-        println!("s: {}", s); // 여기서 s의 소유권을 가져간다.
-    }; // 할당문이므로 ; 이 필요하다
+    println!("이메일을 입력해주세요.");
+    let mut email = String::new();
+    io::stdin().read_line(&mut email);
+    let email = email.trim().to_string(); // 공백 제거
 
-    f();
-    //println!("s: {}", s); // 컴파일 오류: s의 소유권이 없습니다.
+    let stu = create_student(id, name, email); // 학생 인스턴스 생성
+    println!("학번={}, 이름={}, 이메일={}", stu.id, stu.name, stu.email); // 인스턴스의 내부값 참조
+    println!("stu={:?}", stu); // :? 는 Debug 포맷을 기본 출력 포맷으로 지정한다.
+}
+
+#[derive(Debug)] // 이 어노테이션은 구조체의 디버그 포메터를 지정해준다. 없으면 println!("stu={:?}", stu);에서 에러가 발생한다.
+struct Student {
+    id: i32,
+    name: String,
+    email: String,
+}
+
+fn create_student(id: i32, name: String, email: String) -> Student {
+    Student {
+        // Student 인스턴스 생성
+        id: id,
+        name: name,
+        email: email,
+    } // return 값은 ; 이 없다.
 }
