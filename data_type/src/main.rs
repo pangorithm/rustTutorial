8114421e68109253e898bb48aab42843583c4773
pangorithm <phs559800@gmail.com>
2024-07-27 21:40:02 +0900

    String 타입

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,16 +1,6 @@
 fn main() {
-    // 문자열 리터럴을 사용
-    let s: &str = "Hello 러스트!"; // 메모리에 저장된 문자열의 주소를 참조로 사용하기 때문에 항상 &str 형태로 사용된다.
-
-    println!("문자열: {}", s);
-
-    // 문자열 슬라이싱
-    let slice: &str = &s[0..5];
-    println!("슬라이스: {}", slice);
-
-    // 문자열 변환
-    let str: &str = "  Hello Rust  ";
-    println!("{}", str.trim());
-    println!("{}", str.to_lowercase());
-    println!("{}", str.to_uppercase());
+    let mut s = String::from("Hello"); // Hello로 String 생성
+    println!("{}", s);
+    s.push_str(" Rust!"); // s뒤에 Rust! 추가
+    println!("{}", s);
 }
