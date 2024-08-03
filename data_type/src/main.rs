16af150582c527456a91f45fcc4386f13c70521e
pangorithm <phs559800@gmail.com>
2024-08-03 21:39:21 +0900

    링크드 리스트에서 RefCell을 사용하도록 리팩토링

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,36 +1,42 @@
+use std::cell::RefCell; // RefCell은 내부 가변성을 제공하며, 런타임에 빌림 규칙을 검사한다.
 use std::rc::Rc;
 
 struct Node {
     name: String,
-    value: i32,             // 노드의 값
-    next: Option<Rc<Node>>, // Option을 사용해 다음 노드가 없는 상황을 처리
+    value: i32,
+    next: RefCell<Option<Rc<Node>>>, // RefCell로 래핑되어 있기 때문에 불변 참조에서도 수정 가능
 }
 
-fn push_front(head: Rc<Node>, name: String, value: i32) -> Rc<Node> {
+fn push_back(tail: Rc<Node>, name: String, value: i32) -> Rc<Node> {
     let n = Rc::new(Node {
         name: name,
         value: value,
-        next: Some(head.clone()),
+        next: RefCell::new(None),
     });
 
-    n.clone() // 새로 생성된 노드의 Rc를 클론해 반환한다. 이제 이 노드가 새로운 head이다.
+    // tail의 next 필드에 대한 가변 참조를 얻는다.
+    let mut next = tail.next.borrow_mut();
+    *next = Some(n.clone()); // p1 뒤에 p2를 추가해 연결 리스트를 연결
+
+    n // 새로 생성된 노드를 반환한다. 이제 이 노드가 리스트의 마지막 노드이다.
 }
 
 fn main() {
     let head = Rc::new(Node {
         name: String::from("Luna"),
         value: 30,
-        next: None,
+        next: RefCell::new(None),
     });
 
-    let head = push_front(head, String::from("Rust"), 10);
-    let head = push_front(head, String::from("Wikibooks"), 20);
+    let tail = push_back(head.clone(), String::from("Rust"), 10);
+    let tail = push_back(tail, String::from("Wikibooks"), 20);
 
     let mut current = head.clone();
 
     loop {
         print!("{} -> ", current.name);
-        current = match &current.next {
+        let t = current.clone(); // 다음 노드를 참조하기 위해 현재 노드를 복제
+        current = match &(*(t.next.borrow_mut())) {
             Some(n) => n,
             None => break,
         }
