11b2e1f185cc698b08e50c06068cba42bd5df7f5
pangorithm <phs559800@gmail.com>
2024-08-03 21:04:39 +0900

    Rc를 사용하여 linked list 구현

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,45 +1,30 @@
+use std::rc::Rc;
+
 struct Node {
-    value: i32,              // 노드의 값
-    next: Option<Box<Node>>, // Option을 사용해 다음 노드가 없는 상황을 처리
-}
-
-impl Node {
-    fn append(&mut self, elem: i32) {
-        match self.next {
-            Some(ref mut next) => {
-                // 값이 있는 경우
-                next.append(elem); // 마지막 노드를 찾기 위해 다음 노드로 이동
-            }
-            None => {
-                // 값이 없을 경우 마지막 노드로 간주
-                let node = Node {
-                    // 마지막 노드에 값을 삽입
-                    value: elem,
-                    next: None,
-                };
-                self.next = Some(Box::new(node))
-            }
-        }
-    }
-
-    fn list(&self) {
-        print!("{},", self.value);
-        match self.next {
-            Some(ref next) => next.list(), // 다음 노드로 이동
-            None => {}
-        }
-    }
+    name: String,
+    value: i32,             // 노드의 값
+    next: Option<Rc<Node>>, // Option을 사용해 다음 노드가 없는 상황을 처리
 }
 
 fn main() {
-    let mut head = Node {
-        value: 1,
+    let n1 = Rc::new(Node {
+        name: String::from("Luna"),
+        value: 30,
         next: None,
+    });
+
+    let n2 = Rc::new(Node {
+        name: String::from("Rust"),
+        value: 20,
+        next: Some(n1.clone()), // Rc::clone을 사용해 참조 카운트를 증가시킴
+    });
+
+    print!("{} -> ", n2.name); // n2의 이름 출력
+
+    match &n2.next {
+        Some(n) => {
+            println!("{}", n.name); // 다음 노드가 있다면 그 이름을 출력
+        }
+        None => {} // 다음 노드가 없다면 아무것도 하지 않음
     };
-
-    for i in 2..10 {
-        head.append(i);
-    }
-
-    head.list();
 }
