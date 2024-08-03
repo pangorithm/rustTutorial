e13772604e61451c643c0ef05b71a8f9306e30ac
pangorithm <phs559800@gmail.com>
2024-08-03 21:12:03 +0900

    Rc를 활용한 연결리스트 리팩토링

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -6,25 +6,34 @@ struct Node {
     next: Option<Rc<Node>>, // Option을 사용해 다음 노드가 없는 상황을 처리
 }
 
+fn push_front(head: Rc<Node>, name: String, value: i32) -> Rc<Node> {
+    let n = Rc::new(Node {
+        name: name,
+        value: value,
+        next: Some(head.clone()),
+    });
+
+    n.clone() // 새로 생성된 노드의 Rc를 클론해 반환한다. 이제 이 노드가 새로운 head이다.
+}
+
 fn main() {
-    let n1 = Rc::new(Node {
+    let head = Rc::new(Node {
         name: String::from("Luna"),
         value: 30,
         next: None,
     });
 
-    let n2 = Rc::new(Node {
-        name: String::from("Rust"),
-        value: 20,
-        next: Some(n1.clone()), // Rc::clone을 사용해 참조 카운트를 증가시킴
-    });
+    let head = push_front(head, String::from("Rust"), 10);
+    let head = push_front(head, String::from("Wikibooks"), 20);
 
-    print!("{} -> ", n2.name); // n2의 이름 출력
+    let mut current = head.clone();
 
-    match &n2.next {
-        Some(n) => {
-            println!("{}", n.name); // 다음 노드가 있다면 그 이름을 출력
+    loop {
+        print!("{} -> ", current.name);
+        current = match &current.next {
+            Some(n) => n,
+            None => break,
         }
-        None => {} // 다음 노드가 없다면 아무것도 하지 않음
-    };
+        .clone();
+    }
 }
