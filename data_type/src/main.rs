68cf92223e489b1adbd3ac3bba3034eb11505afa
pangorithm <phs559800@gmail.com>
2024-08-03 22:53:04 +0900

    정적 변수

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,28 +1,7 @@
-// 런타임에 판단해 빌림을 반환하는 케이스
-// x, y, 반환 타입 모두 소멸 시점이 명확히 드러나지 않아 컴파일 오류 발생
-// error[E0106]: missing lifetime specifier
-/*
-fn longest(x: &str, y: &str) -> &str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
-}
-*/
-
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
-}
+static GLOBAL_CONST: i32 = 10; // 프로그램이 종료될 때까지 메모리에서 해제되지 않음
 
 fn main() {
-    let s1 = String::from("Hello");
-    let s2 = String::from("Rust");
-
-    let result = longest(&s1, &s2);
-    println!("{}와 {} 중 더 긴 문자열은 '{}'", s1, s2, result); // Hello와 Rust 중 더 긴 문자열은 'Hello'
+    let x: &'static str = "Hello Rust!"; // x는 프로그램이 종료될 때까지 메모리에서 해제되지 않음
+    println!("x: {}", x);
+    println!("GLOBAL_CONST: {}", GLOBAL_CONST);
 }
