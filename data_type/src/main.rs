a0bb22e494bd2a6746ff0b0392b8d48144b52207
pangorithm <phs559800@gmail.com>
2024-08-03 22:18:39 +0900

    약한 참조를 사용한 순환 참조 해결

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,9 +1,9 @@
 use std::cell::RefCell;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
 struct Person {
     id: i32,
-    next: RefCell<Option<Rc<Person>>>,
+    next: RefCell<Option<Weak<Person>>>,
 }
 
 // 참조 횟수가 0이 되는 순간 호출됨
@@ -24,23 +24,26 @@ fn main() {
         next: RefCell::new(None),
     });
 
-    let mut p3 = Rc::new(Person {
-        id: 3,
-        next: RefCell::new(None),
-    });
+    println!(
+        "p1 RefCount: {}, p2 RefCount: {}",
+        Rc::strong_count(&p1),
+        Rc::strong_count(&p2)
+    );
+    // p1 RefCount:1, p2 RefCount:1
 
     // 순환참조 발생
     let mut next = p1.next.borrow_mut();
-    *next = Some(p2.clone());
+    *next = Some(Rc::downgrade(&p2)); // Rc 타입의 참조를 Weak 타입으로 변환한다.
 
     let mut next = p2.next.borrow_mut();
-    *next = Some(p1.clone());
+    *next = Some(Rc::downgrade(&p2)); // Rc 타입의 참조를 Weak 타입으로 변환한다.
 
     println!(
         "p1 RefCount: {}, p2 RefCount: {}",
         Rc::strong_count(&p1),
         Rc::strong_count(&p2)
     );
-    // p1 RefCount:2, p2 RefCount:2
-    // p3 Drop!
+    // p1 RefCount:1, p2 RefCount:1
+    // p2 Drop!
+    // p1 Drop!
 }
