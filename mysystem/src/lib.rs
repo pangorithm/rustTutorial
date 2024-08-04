5bb9fe86d6d90b795d17312bb8fbd0a765cf4c9e
pangorithm <phs559800@gmail.com>
2024-08-04 12:26:06 +0900

    모듈 참조 허용
    private으로 정의되어 있어 참조가 불가능하기 때문에 public으로 정의한다.
    테스트 모드는 println결과를 출력하지 않으므로 `cargo test -- --nocapture` 옵션을 사용해야 출력된다.

--- mysystem/src/lib.rs
+++ mysystem/src/lib.rs
@@ -1,31 +1,31 @@
 // 프레젠테이션 레이어를 나타내는 모듈
-mod presentation {
+pub mod presentation {
     // 뷰에 관련된 기능을 담당하는 모듈
-    mod view {
+    pub mod view {
         // 렌더링에 관련된 함수
-        fn render() {
+        pub fn render() {
             println!("mysystem::presentation::view::render");
         }
     }
 }
 
 // 비즈니스 로직을 담당하는 모듈
-mod business {
+pub mod business {
     // 사용자 관련 비즈니스 로직을 담당하는 모듈
-    mod user {
+    pub mod user {
         // 사용자 생성에 관련된 함수
-        fn create() {
+        pub fn create() {
             println!("mysystem::business::user::create");
         }
     }
 }
 
 // 데이터베이스 작업을 담당하는 모듈
-mod database {
+pub mod database {
     // 사용자 데이터 액세스 객체(DAO)를 나타내는 모듈
-    mod user_dao {
+    pub mod user_dao {
         // 사용자 생성에 관련된 데이터베이스 함수
-        fn create() {
+        pub fn create() {
             println!("mysystem::database::user_dao::create");
         }
     }
