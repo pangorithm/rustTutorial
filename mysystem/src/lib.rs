33e40f860fd929f2abcce4d22d5b5308bcdb3c3d
pangorithm <phs559800@gmail.com>
2024-08-04 12:50:21 +0900

    가시성 제어

--- mysystem/src/lib.rs
+++ mysystem/src/lib.rs
@@ -8,3 +8,17 @@ fn it_works() {
     business::user::create();
     database::user_dao::create();
 }
+
+pub mod my_module {
+    // 이 함수는 외부에서 접근 가능합니다.
+    pub fn public_fn() {}
+
+    // 이 함수는 my_module에서만 접근 가능합니다.
+    fn public_fn() {}
+
+    // 이 함수는 현재 크레이트에 정보를 제공합니다.
+    pub(crate) fn public_fn() {}
+
+    // 이 함수는 상위 모듈에 정보를 제공합니다.
+    pub(super) fn public_fn() {}
+}
