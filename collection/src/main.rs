88428db537c35eccc5c1c0d6568e052373147f41
pangorithm <phs559800@gmail.com>
2024-08-10 21:32:54 +0900

    벡터의 값 변경

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,13 +1,15 @@
 fn main() {
-    let v = vec![1, 2, 3]; // i32 타입의 벡터 v를 생성하고 초깃값으로 1, 2, 3을 설정한다.
-    let one = v[0]; // 벡터의 첫 번째 요소를 one에 할당한다.
-    let two = v.get(1); // 벡터의 두 번째 요소를 가져온다. Option 타입을 반환하므로 결과는 Some(2)이다.
-    let nine = v.get(9); // 벡터 크기를 벗어나는 인덱스
+    let mut v = vec![1, 2, 3]; // i32 타입의 벡터 v를 생성하고 초깃값으로 1, 2, 3을 설정한다.
+    v[0] = 2;
+    v[1] = 3;
+    v[2] = 4;
 
-    println!("One: {:?}, Two: {:?}, Nine: {:?}", one, two, nine);
-    // One: 1, Two: Some(2), Nine: None
+    println!("{}, {}, {}", v[0], v[1], v[2]);
+    // 2, 3, 4
 
-    println!("{:?}", v[9]); // panic! 발생
-                            // thread 'main' panicked at src/main.rs:9:29:
-                            // index out of bounds: the len is 3 but the index is 9
+    for i in &mut v {
+        *i += 1;
+    }
+    println!("{}, {}, {}", v[0], v[1], v[2]);
+    // 3, 4, 5
 }
