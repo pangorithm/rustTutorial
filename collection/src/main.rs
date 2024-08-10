821c538314898b481812dcdbe87eda9d00f70ec2
pangorithm <phs559800@gmail.com>
2024-08-10 21:30:02 +0900

    벡터 개별 요소 읽기

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,16 +1,13 @@
 fn main() {
-    // let mut v: Vec<i32> = Vec::new(); // 빈 i32 타입의 벡터 v를 생성한다.
-    // let mut v: Vec<i32> = vec![1, 2, 3]; // 초기값을 가지는 벡터
-    let mut v: Vec<i32> = vec![]; // 빈 벡터
+    let v = vec![1, 2, 3]; // i32 타입의 벡터 v를 생성하고 초깃값으로 1, 2, 3을 설정한다.
+    let one = v[0]; // 벡터의 첫 번째 요소를 one에 할당한다.
+    let two = v.get(1); // 벡터의 두 번째 요소를 가져온다. Option 타입을 반환하므로 결과는 Some(2)이다.
+    let nine = v.get(9); // 벡터 크기를 벗어나는 인덱스
 
-    for i in 1..10 {
-        // 1부터 9 까지의 숫자를 반복한다.
-        v.push(i);
-    }
+    println!("One: {:?}, Two: {:?}, Nine: {:?}", one, two, nine);
+    // One: 1, Two: Some(2), Nine: None
 
-    for d in &v {
-        // 벡터 v의 각 요소에 대해 반복한다.
-        print!("{}, ", d);
-    }
-    // 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    println!("{:?}", v[9]); // panic! 발생
+                            // thread 'main' panicked at src/main.rs:9:29:
+                            // index out of bounds: the len is 3 but the index is 9
 }
