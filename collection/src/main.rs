ae56b40d971d3368bd4465a17e93dadcf5c2ff83
pangorithm <phs559800@gmail.com>
2024-08-10 21:41:48 +0900

    LikedList

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,15 +1,32 @@
+use std::collections::LinkedList;
+
 fn main() {
-    let mut v = vec![1, 2, 3]; // i32 타입의 벡터 v를 생성하고 초깃값으로 1, 2, 3을 설정한다.
-    v[0] = 2;
-    v[1] = 3;
-    v[2] = 4;
+    let mut list: LinkedList<i32> = LinkedList::new(); // i32 타입의 빈 연결 리스트 생성
+    list.push_back(1); // 리스트의 뒤에 숫자 1을 추가
+    list.push_back(2); // 리스트의 뒤에 숫자 2을 추가
+    list.push_back(3); // 리스트의 뒤에 숫자 3을 추가
 
-    println!("{}, {}, {}", v[0], v[1], v[2]);
-    // 2, 3, 4
-
-    for i in &mut v {
-        *i += 1;
+    for i in &list {
+        print!("{}, ", i);
     }
-    println!("{}, {}, {}", v[0], v[1], v[2]);
-    // 3, 4, 5
+    // 1, 2, 3,
+
+    list = LinkedList::new();
+    for i in 0..10 {
+        list.push_back(i);
+    }
+
+    // 9번째 인덱스 찾기
+    let idx = 9;
+    let mut i = 0;
+    let mut target: i32 = 0;
+
+    for data in &list {
+        if i == idx {
+            target = *data;
+        }
+
+        i += 1;
+    }
+    println!("target: {:?}", target);
 }
