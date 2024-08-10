17dce33253199eaef136d54a20fec3719d9e54dd
pangorithm <phs559800@gmail.com>
2024-08-10 22:04:14 +0900

    HashSet

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,38 +1,31 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main() {
-    let mut books: HashMap<i32, String> = HashMap::new(); // i32 타입의 key와 String 타입의 값을 가지는 빈 HashMap 생성
+    let mut book: HashSet<String> = HashSet::new(); // String 타입의 값을 가지는 빈 HashSet 생성
 
-    books.insert(10, String::from("Rust"));
-    books.insert(20, String::from("Java"));
-    books.insert(30, String::from("Python"));
-
-    for (key, value) in &books {
-        println!("Key: {:?}, Value: {:?}", key, value);
+    book.insert(String::from("Rust"));
+    book.insert(String::from("Java"));
+    book.insert(String::from("Python"));
+    for data in &book {
+        println!("data: {:?}", data);
     }
+    // 조회 순서는 보장되지 않는다
     // # cargo run
-    //    Compiling collection v0.1.0 (/root/git/rustTutorial/collection)
-    //     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.12s
-    //      Running `target/debug/collection`
-    // Key: 10, Value: "Rust"
-    // Key: 30, Value: "Python"
-    // Key: 20, Value: "Java"
+    // data: "Java"
+    // data: "Python"
+    // data: "Rust"
     // # cargo run
-    //     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
-    //      Running `target/debug/collection`
-    // Key: 30, Value: "Python"
-    // Key: 10, Value: "Rust"
-    // Key: 20, Value: "Java"
+    // data: "Rust"
+    // data: "Java"
+    // data: "Python"
     // # cargo run
-    //     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
-    //      Running `target/debug/collection`
-    // Key: 20, Value: "Java"
-    // Key: 30, Value: "Python"
-    // Key: 10, Value: "Rust"
+    // data: "Python"
+    // data: "Rust"
+    // data: "Java"
 
-    // 조회 순서는 보장되지 않는다.
-
-    let rust = books.get(&10);
-    println!("key 10은 {:?}", rust);
-    // key 10은 Some("Rust")
+    // 값이 있는지 확인하기
+    if book.contains("JavaScript") == false {
+        println!("JavaScript가 없습니다.");
+        // JavaScript가 없습니다.
+    }
 }
