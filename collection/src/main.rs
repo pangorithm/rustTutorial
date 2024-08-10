ef024176b5d14292913846919b52c13b34a2cf76
pangorithm <phs559800@gmail.com>
2024-08-10 21:52:06 +0900

    리스트 내부의 값 변경

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -2,36 +2,17 @@ use std::collections::LinkedList;
 
 fn main() {
     let mut list: LinkedList<i32> = LinkedList::new(); // i32 타입의 빈 연결 리스트 생성
-    list.push_back(1); // 리스트의 뒤에 숫자 1을 추가
-    list.push_back(2); // 리스트의 뒤에 숫자 2을 추가
-    list.push_back(3); // 리스트의 뒤에 숫자 3을 추가
-
-    for i in &list {
-        print!("{}, ", i);
-    }
-    // 1, 2, 3,
-    println!("");
-
-    list = LinkedList::new();
     for i in 0..10 {
         list.push_back(i);
     }
 
-    // 9번째 인덱스 찾기
-    let idx = 9;
-    let mut i = 0;
-    let mut target: i32 = 0;
-
-    for data in &list {
-        if i == idx {
-            target = *data;
-        }
-
-        i += 1;
+    for d in list.iter_mut() {
+        // 수정 가능한 반복자를 얻는다.
+        *d += 10;
     }
-    println!("target: {:?}", target);
-    // target: 9
 
-    println!("iterator target: {:?}", list.iter().nth(9));
-    // iterator target: Some(9)
+    for d in list.iter() {
+        print! {"{:?}, ", d};
+    }
+    // 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 }
