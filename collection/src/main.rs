f92bf129e0e0b01a42fc1a376db70b5041f85ce2
pangorithm <phs559800@gmail.com>
2024-08-10 22:51:24 +0900

    문자열 만들기

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,15 +1,9 @@
-use std::collections::BinaryHeap;
-
 fn main() {
-    let mut heap: BinaryHeap<i32> = BinaryHeap::new(); // String 타입의 값을 가지는 빈 HashSet 생성
+    let mut eng = String::new();
+    eng.push_str("hello");
+    let chn = "你好".to_string();
+    let kor = String::from("안녕하세요");
 
-    heap.push(3);
-    heap.push(9);
-    heap.push(2);
-    heap.push(5);
-
-    while heap.is_empty() == false {
-        print!("{:?}, ", heap.pop()); // 힙의 최대값을 꺼내어 출력한다. pop()은 Option<T>를 반환하므로 {:?}를 사용해 출력한다.
-    }
-    // Some(9), Some(5), Some(3), Some(2),
+    println!("{}, {}, {}", eng, chn, kor);
 }
+// hello, 你好, 안녕하세요
