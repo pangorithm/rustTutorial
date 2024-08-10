72456d2ba17f6ecf91350ebd35f6d7a7500887b1
pangorithm <phs559800@gmail.com>
2024-08-10 22:00:00 +0900

    HashMap

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,18 +1,38 @@
-use std::collections::LinkedList;
+use std::collections::HashMap;
 
 fn main() {
-    let mut list: LinkedList<i32> = LinkedList::new(); // i32 타입의 빈 연결 리스트 생성
-    for i in 0..10 {
-        list.push_back(i);
-    }
+    let mut books: HashMap<i32, String> = HashMap::new(); // i32 타입의 key와 String 타입의 값을 가지는 빈 HashMap 생성
 
-    for d in list.iter_mut() {
-        // 수정 가능한 반복자를 얻는다.
-        *d += 10;
-    }
+    books.insert(10, String::from("Rust"));
+    books.insert(20, String::from("Java"));
+    books.insert(30, String::from("Python"));
 
-    for d in list.iter() {
-        print! {"{:?}, ", d};
+    for (key, value) in &books {
+        println!("Key: {:?}, Value: {:?}", key, value);
     }
-    // 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    // # cargo run
+    //    Compiling collection v0.1.0 (/root/git/rustTutorial/collection)
+    //     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.12s
+    //      Running `target/debug/collection`
+    // Key: 10, Value: "Rust"
+    // Key: 30, Value: "Python"
+    // Key: 20, Value: "Java"
+    // # cargo run
+    //     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
+    //      Running `target/debug/collection`
+    // Key: 30, Value: "Python"
+    // Key: 10, Value: "Rust"
+    // Key: 20, Value: "Java"
+    // # cargo run
+    //     Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
+    //      Running `target/debug/collection`
+    // Key: 20, Value: "Java"
+    // Key: 30, Value: "Python"
+    // Key: 10, Value: "Rust"
+
+    // 조회 순서는 보장되지 않는다.
+
+    let rust = books.get(&10);
+    println!("key 10은 {:?}", rust);
+    // key 10은 Some("Rust")
 }
