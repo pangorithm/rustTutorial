b0470a1c48c1673dfedfeec0f5ac3a79bcb7806a
pangorithm <phs559800@gmail.com>
2024-08-10 23:16:21 +0900

    컬렉션의 소유권 into_iter()

--- collection/src/main.rs
+++ collection/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     let vec = vec![1, 2, 3];
-    for item in vec.iter() {
-        // vec의 불변 빌림 반복자 생성
+    for item in vec.into_iter() {
+        // vec의 소유권은 이동했으므로 이후에 vec을 사용할 수 없음
         println!("{}", item);
     }
 
-    println!("{:?}", vec); // vec 접근 가능
-                           // [1, 2, 3]
+    // println!("{:?}", vec);
+    // 8   |     println!("{:?}", vec);
+    //     |                      ^^^ value borrowed here after move
 }
