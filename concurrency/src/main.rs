72a9673f7a315613e5d7c3fb0629ec757dd43ec1
pangorithm <phs559800@gmail.com>
2024-10-20 13:17:46 +0900

    스레드를 사용해 파일 읽기

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,9 +1,17 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::thread;
 
 fn main() {
-    // 새로운 스레드를 생성하고 실행
     let handle = thread::spawn(|| {
-        println!("스레드에서 실행"); // 새로 생성된 스레드에서 출력
+        // 새로운 스레드를 생성하고 그 핸들을 받습니다.
+        let file = File::open("file.txt").unwrap(); // "file.txt" 파일을 엽니다.
+        let reader = BufReader::new(file); // 버퍼링을 사용해 파일을 읽습니다.
+        for line in reader.lines() {
+            // 파일의 각 줄을 순회합니다.
+            let txt = line.unwrap(); // 줄을 텍스트로 읽습니다.
+            println!("{}", txt);
+        }
     });
 
     handle.join().unwrap(); // 스레드가 완료될 때까지 대기 (종료 대기)
