3444909ca5d11c2999ce7e7629b07fdfb543d1fa
pangorithm <phs559800@gmail.com>
2024-10-20 14:20:44 +0900

    간단한 이벤트 루프 예제

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,34 +1,19 @@
-use tokio::time;
-use std::time::Duration;
+use std::io;
 
-async fn sleep_10sec() {
-    for i in 1..10 {
-        println!(".");
-        time::sleep(Duration::from_millis(1000)).await; // 1초간 10회 대기
+fn main() {
+    println!("아무 내용이나 입력하세요. quit를 입력하면 종료됩니다.");
+
+    loop {
+        // 무한 반복해 이벤트를 처리
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+
+        let input = buf.trim();
+        if input == "quit" {
+            // 이벤틀을 처리합니다.
+            break;
+        }
+
+        println!("입력: {}", input);
     }
-}
-
-async fn calc_sum(start: i32, end: i32) -> i32 {
-    let mut sum = 0;
-
-    for i in start..=end {
-        println!("{} ", i);
-        sum += i;
-    }
-
-    sum
-}
-
-async fn calc() -> i32 {
-    let f1 = sleep_10sec();
-    let f2 = calc_sum(1, 10);
-    let (_, sum) = tokio::join!(f1, f2); // f1과 f2가 끝나기를 기다린다.
-
-    sum
-}
-
-#[tokio::main]
-async fn main() {
-    let sum = calc().await;
-    println!("1부터 10까지의 합: {}", sum);
 }
