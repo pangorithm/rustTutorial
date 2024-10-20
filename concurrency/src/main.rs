678aab8e42fb91aaf0670010f91fe26f6c99265b
pangorithm <phs559800@gmail.com>
2024-10-20 13:45:44 +0900

    await 키워드: async 함수 안에서 async 함수 호출하기

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -11,8 +11,16 @@ async fn calc_sum(start: i32, end: i32) -> i32 {
     sum
 }
 
+async fn calc() -> i32 {
+    let sum1_50 = calc_sum(1, 50).await;        // await 키워드를 사용해 결과를 얻습니다.
+    let sum51_100 = calc_sum(51, 100).await;    // await 키워드를 사용해 결과를 얻습니다.
+    let ret = sum1_50 + sum51_100;
+
+    ret
+}
+
 fn main() {
-    let future = calc_sum(1, 100);
+    let future = calc();
 
     let sum = block_on(future);
     println!("1부터 100까지의 합: {}", sum);
