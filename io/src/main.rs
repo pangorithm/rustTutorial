394ea8378c9439917b9719969525261e2af17240
pangorithm <phs559800@gmail.com>
2024-10-26 14:52:45 +0900

    비동기식 입출력

--- io/src/main.rs
+++ io/src/main.rs
@@ -1,15 +1,14 @@
-use std::fs::File;
-use std::io::{Read, Write};
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-fn main() {
-    let mut file = File::open("input.txt").unwrap();
+#[tokio::main]
+async fn main() {
+    let mut file = File::open("input.txt").await.unwrap(); // 비동기 방식으로 file handel을 얻습니다.
     let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    // 파일을 읽을 때까지 대기합니다.
+    file.read_to_string(&mut contents).await.unwrap(); // 비동기 방식으로 file을 읽습니다.
 
     println!("{}", contents);
 
-    let mut file = File::create("output.txt").unwrap();
-    file.write_all(contents.as_bytes()).unwrap();
-    // 파일을 쓸 때까지 대기합니다.
+    let mut file = File::create("output.txt").await.unwrap(); // 비동기 방식으로 file을 생성합니다.
+    file.write_all(contents.as_bytes()).await.unwrap(); // 비동기 방식으로 file에 내용을 저장합니다.
 }
