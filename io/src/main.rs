4b2cdf46b2fa1303fbfa9409ca6689ea0f77b157
pangorithm <phs559800@gmail.com>
2024-10-26 15:03:11 +0900

    비동기식 이벤트 루프 입력

--- io/src/main.rs
+++ io/src/main.rs
@@ -1,14 +1,24 @@
 use tokio::fs::File;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::io::{stdin, BufReader, AsyncBufReadExt};
 
 #[tokio::main]
 async fn main() {
-    let mut file = File::open("input.txt").await.unwrap(); // 비동기 방식으로 file handel을 얻습니다.
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).await.unwrap(); // 비동기 방식으로 file을 읽습니다.
+    let mut reader = BufReader::new(stdin());
+    let mut lines = reader.lines();
 
-    println!("{}", contents);
+    loop { // quit가 입력될 때까지 입력을 받음
+        match lines.next_line().await.unwrap() {
+            Some(input) => {
+                println!("입력: {}", input);
 
-    let mut file = File::create("output.txt").await.unwrap(); // 비동기 방식으로 file을 생성합니다.
-    file.write_all(contents.as_bytes()).await.unwrap(); // 비동기 방식으로 file에 내용을 저장합니다.
+                if input == "quit" { // quit을 입력받으면 종료합니다.
+                    break;
+                }
+            }
+            None => {
+                break;
+            }
+        };
+
+    }
 }
