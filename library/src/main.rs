f972e351cc9cc6f311b8a3a86770cc4e87c6f83c
pangorithm <phs559800@gmail.com>
2024-10-28 21:49:29 +0900

    Drop 트레잇

--- library/src/main.rs
+++ library/src/main.rs
@@ -1,31 +1,17 @@
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u32,
-    cloned: bool,
+struct Book {
+    title: String,
 }
 
-// Person을 복제한다. Copy과는 다르게 값을 직접 설정할 수 있다
-impl Clone for Person {
-    fn clone(&self) -> Self {
-        Person {
-            name: self.name.clone(),
-            age: self.age,
-            cloned: true,
-        }
+impl Drop for Book {
+    fn drop(&mut self) {
+        println!("Book 객체 해제: {}", self.title);
     }
 }
 
 fn main() {
-    let person1 = Person {
-        name: String::from("루나"),
-        age: 10,
-        cloned: false,
-    };
-
-    // person1을 복제합니다. 소유권을 잃지 않습니다.
-    let person2 = person1.clone();
-
-    println!("{:?}", person1);
-    println!("{:?}", person2);
+    {
+        let book = Book {
+            title: String::from("러스트"),
+        };
+    } // book이 스코프를 벗어나며 book의 Drop 트레잇이 자동으로 호출된다.
 }
