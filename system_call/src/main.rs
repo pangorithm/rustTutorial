045f6186ee9fc3807fed44dcdb01fb29a12f1555
pangorithm <phs559800@gmail.com>
2024-10-30 20:54:30 +0900

    use std::process

--- system_call/src/main.rs
+++ system_call/src/main.rs
@@ -1,15 +1,18 @@
-use std::env;
+use std::process;
+use std::process::Command;
 
 fn main() {
-    match env::current_dir() {
-        Ok(path) => println!("현재 경로: {:?}", path),
-        Err(e) => println!("현재 경로 획득 실패: {}", e),
-    }
+    // 쉘에서 echo를 실행
+    let echo = Command::new("echo")
+        .arg("echo 실행") // "echo 실행"이라는 인자를 추가한다.
+        .output()
+        .expect("echo 실행 실패");
 
-    match env::temp_dir().to_str() {
-        Some(path) => println!("임시 경로: {}", path),
-        None => println!("임시경로 확인 불가"),
-    }
+    // 명령어의 출력을 UTF-8 문자열로 변환
+    let ret = String::from_utf8_lossy(&echo.stdout);
+
+    println!("결과: {}", ret);
+
+    let pid = process::id(); // 현재 실행중인 프로세스의 pid 획득
+    println!("Process ID: {}", pid);
 }
-// 현재 경로: "/root/git/rustTutorial/system_call"
-// 임시 경로: /tmp
