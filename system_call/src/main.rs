a3603f229c6375972f7b578a8c2884e53fd3b293
pangorithm <phs559800@gmail.com>
2024-10-30 20:37:51 +0900

    std::env 명령줄 인자 확인

--- system_call/src/main.rs
+++ system_call/src/main.rs
@@ -1,19 +1,14 @@
-use std::fs::File;
-use std::io::Read;
-use std::os::unix::io::{FromRawFd, IntoRawFd};
+use std::env;
 
 fn main() {
-    // test.txt를 연다.
-    let f = File::open("test.txt").unwrap();
-
-    // f의 파일 서술자를 획득한다
-    let fd = f.into_raw_fd();
-
-    // 파일 서술자로부터 File 객체를 생성한다.
-    let mut f = unsafe { File::from_raw_fd(fd) };
-
-    // 파일 내용을 출력한다.
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("파일 읽기 실패");
-    println!("{}", contents);
+    for (index, argument) in env::args().enumerate() {
+        println!("인자 #{}: {}", index, argument);
+    }
 }
+/*
+# cargo run test
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.00s
+     Running `target/debug/system_call test`
+인자 #0: target/debug/system_call
+인자 #1: test
+*/
