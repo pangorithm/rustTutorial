e8e8e9f2fdebaf65b9314e318955c9e0e1682fdb
pangorithm <phs559800@gmail.com>
2024-11-03 12:13:57 +0900

    다형성

--- OOP/src/main.rs
+++ OOP/src/main.rs
@@ -1,28 +1,36 @@
-struct Student {
-    id: i32,           // private field
-    pub name: String,  // public field
-    pub email: String, // public field
+// Hello라는 트레잇을 정의한다. 이 트레잇은 hello_msg 메서드를 가져야 한다.
+trait Hello {
+    fn hello_msg(&self) -> String; // hello_msg 메서드는 String을 반환해야 하며, 이를 구현하는 타입에서 정의해야 한다.
 }
 
-impl Student {
-    // public 생성자
-    pub fn new(id: i32, name: String, email: String) -> Student {
-        Student { id, name, email }
-    }
+// say_hello 함수는 Hello 트레잇을 구현하는 어떠한 타입의 참조도 받을 수 있다.
+// 이 함수는 전달받은 타입의 hello_msg 메서드를 호출하고 그 결과를 출력한다.
+fn say_hello(say: &dyn Hello) {
+    println!("{}", say.hello_msg());
+}
 
-    // public 메서드
-    pub fn get_name(&self) -> &String {
-        &self.name
-    }
+struct Student {}
 
-    // private 메서드
-    fn set_name(&mut self, name: String) {
-        self.name = name.clone();
+impl Hello for Student {
+    fn hello_msg(&self) -> String {
+        String::from("안녕하세요! 선생님,")
+    }
+}
+
+struct Teacher {}
+
+impl Hello for Teacher {
+    fn hello_msg(&self) -> String {
+        String::from("안녕하세요! 오늘 수업은...")
     }
 }
 
 fn main() {
-    let student = Student::new(1, String::from("luna"), String::from("luna@email.me"));
-    println!("이름: {}", student.get_name());
-    // 이름: luna
+    let student = Student {};
+    let teacher = Teacher {};
+
+    say_hello(&student);
+    // 안녕하세요! 선생님,
+    say_hello(&teacher);
+    // 안녕하세요! 오늘 수업은...
 }
